342bdd97a472497464fb34462fd33017b1da41e7
Kris <>
2020-08-17 03:02:40 +0800

    Feat: init presenter

---
+++ Currency Calculator/Presenter/CurrencyPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyPresenter.swift
+//  Currency Calculator
+//
+//  Created by Kris on 2020/8/17.
+//  Copyright © 2020 KrisLin. All rights reserved.
+//
+
+import Foundation
+
+class Presenter {
+    
+}
