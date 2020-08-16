36e09c871760fae1bface8b6d1a6f2c5cbb5bd31
Kris <>
2020-08-17 01:25:01 +0800

    Init: Create the project

---
+++ Currency Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Calculator
+//
+//  Created by Kris on 2020/8/17.
+//  Copyright © 2020 KrisLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
